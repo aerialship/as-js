a893b3a2e1f6b92bb63f63cc043bfc6afb2a8317
Milos Tomic <tmilos@gmail.com>
2014-11-20 14:45:17 +0100

    load action tests

---
+++ tests/unit/load/load_basic.js
@@ -0,0 +1,99 @@
+(function($) {
+
+    QUnit.module('load:basic');
+
+    QUnit.asyncTest('puts loaded content to result of next call', function(assert) {
+        assert.expect(2);
+
+        var expectedResult = '<p>response</p>';
+
+        $.mockjax({
+            url: 'data1.json',
+            response: function() {
+                assert.ok(true);
+                this.responseText = expectedResult;
+            }
+        });
+
+        AS.container.set('assertResult', function(options) {
+            QUnit.start();
+            assert.equal(options.result, expectedResult);
+        });
+
+        $('#btn1').click();
+
+    });
+
+    QUnit.asyncTest('buffers multiple events into one', function(assert) {
+        assert.expect(2);
+
+        var expectedResult = '<p>response</p>';
+
+        $.mockjax({
+            url: 'data2.json',
+            response: function() {
+                assert.ok(true);
+                this.responseText = expectedResult;
+            }
+        });
+
+        AS.container.set('assertResult', function(options) {
+            QUnit.start();
+            assert.equal(options.result, expectedResult);
+        });
+
+        $('#btn2').click();
+        $('#btn2').click();
+        $('#btn2').click();
+
+    });
+
+    QUnit.asyncTest('adds data to request', function(assert) {
+        assert.expect(2);
+
+        $.mockjax({
+            url: 'data3.json',
+            response: function(settings) {
+                assert.equal(settings.data.a, "aaa");
+                assert.equal(settings.data.b, "bbb");
+                this.responseText = '';
+                QUnit.start();
+            }
+        });
+        $.mockjax({
+            url: /.*/,
+            response: function(settings) {
+                assert.ok(false, 'mismatched url: '+settings.url);
+            }
+        });
+
+        $('#btn3').click();
+
+    });
+
+    QUnit.test('calls block on body', function(assert) {
+        assert.expect(2);
+
+        $.mockjax({
+            url: /.*/,
+            responseText: ''
+        });
+
+        $.blockUI = function() {
+            assert.ok(true);
+            QUnit.start();
+        };
+
+        $.unblockUI = function() {
+            assert.ok(true);
+            QUnit.start();
+        };
+
+        QUnit.stop();
+        QUnit.stop();
+
+        $('#btn4').click();
+
+    });
+
+})(jQuery);
