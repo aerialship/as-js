3299c33669ac1bc335c6dd0183e14ab37a9c8dcd
Milos Tomic <tmilos@gmail.com>
2015-04-08 14:57:53 +0200

    dist 0.3.1

--- dist/as.js
+++ dist/as.js
@@ -416,6 +416,13 @@ AS.container.set('ajax.submit', function(options) {
 });
 
 
+AS.container.set('bs.modal.hide', function(options) {
+    AS.assertTrue(options, ['selector'], 'bs.modal.close');
+
+    $(options.selector).modal('hide');
+});
+
+
 AS.container.set('bs.modal.load', function(options) {
 
     AS.assertTrue(options, ['url'], 'bs.modal.load');
@@ -533,6 +540,26 @@ AS.container.set('class.toggle', function(options) {
 });
 
 
+AS.container.set('data.set', function(options) {
+    AS.assertTrue(options, ['data'], 'data.set');
+
+    var $dom = options.selector ? $(options.selector) : options.$dom,
+        name, value
+        ;
+
+    if (typeof options.data != 'object') {
+        throw new SyntaxError('data.set option.data must be an object');
+    }
+
+    for (name in options.data) {
+        if (options.data.hasOwnProperty(name)) {
+            value = options.data[name];
+            $dom.data(name, value);
+        }
+    }
+});
+
+
 AS.container.set('eval', function(options) {
 
     AS.assertTrue(options.exp);
@@ -578,6 +605,7 @@ AS.container.set('html', function(options) {
     } else {
         $target.html($result);
     }
+    AS.bind($target);
 
     if (options.success) {
         AS.execute($result, options.success);
