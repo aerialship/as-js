7e26a664139bd643991716e22d7e5d773ceac787
Milos Tomic <tmilos@gmail.com>
2015-07-03 12:35:07 +0200

    version 0.3.4

--- dist/as.js
+++ dist/as.js
@@ -585,6 +585,66 @@ AS.container.set('eval', function(options) {
 });
 
 
+AS.container.set('fade', function(options) {
+
+    AS.assertTrue(options, ['type'], 'fade');
+
+    if (options.type != 'in' && options.type != 'out') {
+        throw new SyntaxError('Fade type must be in or out');
+    }
+
+    if (typeof options.target == 'undefined') {
+        options.target = options.dom;
+    }
+    if (typeof options.duration == 'undefined') {
+        options.duration = 300;
+    }
+
+    var oldComplete = options.complete,
+        type = options.type,
+        $target
+    ;
+
+    delete options.type;
+
+    options.complete = function() {
+        AS.log({
+            msg: 'AS - fadeIn - complete',
+            options: options
+        });
+        if (oldComplete) {
+            AS.execute(options.dom, oldComplete);
+        }
+    };
+
+    $target = AS.assertSelector(options.target, 'fade empty target');
+
+    if (type == 'in') {
+        $target.fadeIn(options);
+    } else {
+        $target.fadeOut(options);
+    }
+});
+
+AS.container.set('fadeIn', function(options) {
+
+    options.type = 'in';
+    AS.execute(options.dom, {
+        fade: options
+    }, options.domEvent);
+
+});
+
+AS.container.set('fadeOut', function(options) {
+
+    options.type = 'out';
+    AS.execute(options.dom, {
+        fade: options
+    }, options.domEvent);
+
+});
+
+
 AS.container.set('false', function(options) {
 
     return false;
